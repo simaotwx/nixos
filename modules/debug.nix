b57d5ec8607c5c455c5aaf79550d49cbeba00c9b
Simão Gomes Viana <devel@superboring.dev>
2025-03-24 20:33:47 +0100

    debug: add loglevel settings

--- modules/debug.nix
+++ modules/debug.nix
@@ -10,5 +10,12 @@
   in
   {
     boot.initrd.verbose = customization.debug.enable;
+    boot.kernelParams = if customization.debug.enable then [
+      "rd.systemd-show_status=true"
+    ] else [
+      "quiet" "loglevel=2"
+      "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3"
+    ];
+
   };
 }
