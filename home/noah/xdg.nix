a32c85f8a766fd9ff05ac4cdb28c980031e5dc78
Simão Gomes Viana <devel@superboring.dev>
2025-04-04 22:57:20 +0200

    noah: deduplicate and merge mimeApps associations

--- home/noah/xdg.nix
+++ home/noah/xdg.nix
@@ -1,4 +1,4 @@
-{ ... }: {
+{ config, ... }: {
   xdg = {
     enable = true;
     mimeApps.enable = true;
@@ -21,27 +21,10 @@
         "audio/opus" = "io.github.celluloid_player.Celluloid.desktop";
         "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
         "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
+        "x-sheme-handler/discord" = "vesktop.desktop";
       };
     };
-    mimeApps.defaultApplications = {
-      "application/pdf" = "org.gnome.Evince.desktop";
-      "text/html" = "zen.desktop";
-      "x-scheme-handler/http" = "zen.desktop";
-      "x-scheme-handler/https" = "zen.desktop";
-      "x-scheme-handler/about" = "zen.desktop";
-      "image/png" = "org.gnome.Loupe.desktop";
-      "image/jpg" = "org.gnome.Loupe.desktop";
-      "image/jpeg" = "org.gnome.Loupe.desktop";
-      "image/gif" = "org.gnome.Loupe.desktop";
-      "audio/aac" = "io.github.celluloid_player.Celluloid.desktop";
-      "audio/flac" = "io.github.celluloid_player.Celluloid.desktop";
-      "audio/ogg" = "io.github.celluloid_player.Celluloid.desktop";
-      "audio/wav" = "io.github.celluloid_player.Celluloid.desktop";
-      "audio/opus" = "io.github.celluloid_player.Celluloid.desktop";
-      "x-scheme-handler/tg" = "org.telegram.desktop.desktop";
-      "x-scheme-handler/tonsite" = "org.telegram.desktop.desktop";
-      "x-sheme-handler/discord" = "vesktop.desktop";
-    };
+    mimeApps.defaultApplications = config.xdg.mimeApps.associations.added;
     portal.enable = true;
   };
 }
