e0ae1cc960b1a9f175358a47c2dc495c8b6668eb
Simão Gomes Viana <devel@superboring.dev>
2025-04-23 22:39:36 +0200

    Add pickrangeib command

--- modules/shell.nix
+++ modules/shell.nix
@@ -22,6 +22,10 @@
           git rev-list --reverse --topo-order $1^..$2 | while read rev
           do echo "$rev"; done | xargs git cherry-pick -s
         '')
+        (pkgs.writeShellScriptBin "pickrangeib" ''
+          git rev-list --reverse --topo-order $1^..$2 | while read rev
+          do git cherry-pick -s $rev || git cherry-pick --skip; done
+        '')
       ];
     })
   ]);
