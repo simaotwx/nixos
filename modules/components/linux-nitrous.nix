150fcff5796ffc5fb392700933a7a9174071cdad
Simão Gomes Viana <devel@superboring.dev>
2025-05-03 21:54:27 +0200

    Update linux-nitrous to 6.14.5-1

--- modules/components/linux-nitrous.nix
+++ modules/components/linux-nitrous.nix
@@ -12,7 +12,7 @@
   config = {
     customization.linux-nitrous.cpuVendor = config.customization.hardware.cpu.vendor;
     boot.kernelPackages = lib.mkOverride 80 (let
-        version = "6.14.4-1";
+        version = "6.14.5-1";
         linuxVersion = lib.head (lib.splitString "-" version);
         suffix = "nitrous";
         llvm = pkgs.llvmPackages_19;
@@ -31,7 +31,7 @@
 
             src = fetchurl {
               url = "https://gitlab.com/xdevs23/linux-nitrous/-/archive/v${version}/linux-nitrous-v${version}.tar.gz";
-              hash = "sha256-wOMqOIB1VijJPdGo6hpM0NTm6V3/0q1JvUzaQfXgKGo=";
+              hash = "sha256-E/f0pi1G9lsCUi3K+7Tous2y/yeOJObEtLjuZJphmvU=";
             };
 
             structuredExtraConfig = with lib.kernel; {
