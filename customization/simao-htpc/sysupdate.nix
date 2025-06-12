2394816eee549ee647c568617297406425e98c2c
Simão Gomes Viana <devel@superboring.dev>
2025-06-12 21:14:54 +0200

    Updates to simao-htpc

--- customization/simao-htpc/sysupdate.nix
+++ customization/simao-htpc/sysupdate.nix
@@ -3,34 +3,6 @@
     enable = true;
 
     transfers = {
-      "10-uki" = {
-        Source = {
-          MatchPattern = [
-            "${config.boot.uki.name}_@v.efi.xz"
-            "${config.boot.uki.name}_@v.efi"
-          ];
-
-          # Path = "https://...";
-          # Type = "url-file";
-          Path = "/var/updates/";
-          Type = "regular-file";
-        };
-
-        Target = {
-          MatchPattern = [
-            "${config.boot.uki.name}_@v.efi"
-          ];
-
-          Path = "/EFI/Linux";
-          PathRelativeTo = "boot";
-
-          Type = "regular-file";
-        };
-
-        Transfer = {
-          ProtectVersion = "%A";
-        };
-      };
       "10-uki-remote" = {
         Source = {
           MatchPattern = [
@@ -55,23 +27,6 @@
 
         Transfer = {
           ProtectVersion = "%A";
-        };
-      };
-      "20-store" = {
-        Source = {
-          MatchPattern = [
-            "store_@v.img.xz"
-            "store_@v.img"
-          ];
-
-          Path = "/var/updates/";
-          Type = "regular-file";
-        };
-
-        Target = {
-          Path = config.customization.partitions.systemDisk;
-          MatchPattern = "store_@v";
-          Type = "partition";
         };
       };
       "20-store-remote" = {
