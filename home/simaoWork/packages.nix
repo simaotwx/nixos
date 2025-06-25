95491d055c2e6b17a6ff8d70c30b36b971d01a14
Simao Gomes Viana <simao.gomes@toowoxx.de>
2025-06-25 17:50:59 +0200

    simaoWork: add qt-core extension

--- home/simaoWork/packages.nix
+++ home/simaoWork/packages.nix
@@ -125,5 +125,11 @@ in
         version = "1.5.1";
         sha256 = "sha256-l19OW4lJR8+SxHeLvRzBGtxC+y5seNdOz9jnlK9HDkQ=";
       }
+      {
+        name = "qt-core";
+        publisher = "TheQtCompany";
+        version = "1.5.1";
+        sha256 = "sha256-0I41cw809oeL5n78TkNKJ+YdFBu237vaNBZuWv3xKn8=";
+      }
   ];
 }
