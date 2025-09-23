612fff9091927ed208388f0e1dca084a2a372198
Simão Gomes Viana <devel@superboring.dev>
2025-09-23 22:26:09 +0200

    json2nix: simplify

--- modules/commands.nix
+++ modules/commands.nix
@@ -2,8 +2,7 @@
 {
   environment.systemPackages = [
     (pkgs.writeShellScriptBin "json2nix" ''
-      #!${pkgs.runtimeShell}
-      nix eval --arg-from-stdin stdin --expr "{ stdin }: { output = builtins.fromJSON stdin; }" output | \
+      ${lib.getExe pkgs.nix} eval --arg-from-stdin stdin --expr "{ stdin }: { output = builtins.fromJSON stdin; }" output | \
         ${lib.getExe pkgs.nixfmt-rfc-style}
     '')
   ];
