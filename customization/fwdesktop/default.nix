554673af5887f404840d2f5573e4bbdffffb8c1b
Simao Gomes Viana <simao.gomes@toowoxx.de>
2025-10-21 15:00:09 +0200

    fwdesktop: add vllm

--- customization/fwdesktop/default.nix
+++ customization/fwdesktop/default.nix
@@ -24,6 +24,7 @@
     foundrixModules.config.compat
     foundrixModules.components.ollama
     foundrixModules.components.llama-cpp
+    "${flakePath}/modules/components/vllm.nix"
     ./chat-ui-deployment.nix
   ];
 
