cb29a2eb93a5734fe4336f988bad3cef2f2eedf5
Simão Gomes Viana <devel@superboring.dev>
2025-10-26 00:33:07 +0200

    Migrate gstreamer to foundrix

--- home/julianWork/packages.nix
+++ home/julianWork/packages.nix
@@ -98,15 +98,6 @@ in
       zip
       bmap-tools
 
-      # GStreamer
-      gst_all_1.gstreamer
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad
-      gst_all_1.gst-plugins-ugly
-      gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
-
       # AOSP stuff
       git-repo
       xmlstarlet
--- home/noah/packages.nix
+++ home/noah/packages.nix
@@ -91,15 +91,6 @@ in
       pkgs.gnome-tweaks
       gnome-terminal
 
-      # GStreamer
-      gst_all_1.gstreamer
-      gst_all_1.gst-plugins-base
-      gst_all_1.gst-plugins-good
-      gst_all_1.gst-plugins-bad
-      gst_all_1.gst-plugins-ugly
-      gst_all_1.gst-libav
-      gst_all_1.gst-vaapi
-
       # AOSP stuff
       git-repo
       xmlstarlet
